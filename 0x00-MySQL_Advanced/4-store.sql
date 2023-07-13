c06295b22da697ecb923a9b2b1556612a63993c0
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-13 18:31:27 +0300

    t4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,8 +2,6 @@
 CREATE TRIGGER dec_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-	UPDATE items
-	SET quantity = quantity - NEW.number
-	WHERE name = NEW.item_name;
-END;
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name LIKE NEW.item_name;
