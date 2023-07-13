8bebd95f531b6b353e5324d2ca6f6d595bdc18f6
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-13 22:39:48 +0300

    t7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,10 @@
+-- Creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	DECLARE avg_sc FLOAT;
+
+	SELECT AVG(score) INTO avg_sc FROM corrections GROUP BY user_id;
+	UPDATE users SET average_score = avg_sc WHERE id = user_id;
+END //
+DELIMITER ;
