30a7bd973b631a7bc66eb8c0b365036c3ae442cf
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-13 19:33:16 +0300

    t5 DELIMITER update set

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- Creates a trigger that resets the attribute valid_email only when the email has been changed.
+DELIMITER //
+CREATE TRIGGER valid_em
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email <> OLD.email THEN
+		IF OLD.valid_email = 0 THEN 
+			SET NEW.valid_email = 1;
+		ELSE
+			SET NEW.valid_email = 0;
+		END IF;
+	END IF;
+END //
+DELIMITER ;
