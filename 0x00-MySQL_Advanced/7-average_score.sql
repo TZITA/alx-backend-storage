15261970153f1dd6bff33f682bf26bf1933e8cf6
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-14 11:16:03 +0300

    modified decimal datatype

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,7 +2,7 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE avg_sc DECIMAL;
+	DECLARE avg_sc DECIMAL(10, 2);
 
 	SELECT AVG(score) INTO avg_sc FROM corrections WHERE corrections.user_id = user_id;
 	UPDATE users SET average_score = avg_sc WHERE id = user_id;
