3bf0706a9080f1068760e4abc38ccc591136a7a1
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-17 22:34:54 +0300

    inser doc in python

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,6 +3,6 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-    """ """
+    """Inserts a new doc and returns its id"""
     doc = mongo_collection.insert_one(kwargs)
     return doc.inserted_id
