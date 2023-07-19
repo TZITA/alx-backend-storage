721d5676cd24b2bd45ae8bc5ce620cf856017f8d
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-19 22:52:35 +0300

    decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,14 +15,29 @@ def count_calls(method):
     return wrapper
 
 
+def call_history(method):
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        inputs_key = f"{method.__qualname__}:inputs"
+        outputs_key = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(inputs_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, output)
+
+        return output
+    return wrapper
+
+
 class Cache:
     """Cache class to write strings to Redis"""
     def __init__(self):
         """Initialization"""
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method genrates a random key"""
         key = str(uuid4())
